88e3a03693e45e1c2b35787630e4f83180be1774
Jose Luis Cuevas <cuevas.joseluis@outlook.com>
2021-08-29 20:55:20 -0700

    Agregar archivos de proyecto.

---
+++ GraphQLDotNetCore/GraphQL/GraphQLSchema/AppSchema.cs
@@ -0,0 +1,13 @@
+﻿using GraphQL.Types;
+using System;
+
+namespace GraphQLNet5.GraphQL.GraphQLSchema
+{
+    public class AppSchema : Schema
+    {
+        public AppSchema(IServiceProvider provider)
+            : base(provider)
+        {
+        }
+    }
+}
